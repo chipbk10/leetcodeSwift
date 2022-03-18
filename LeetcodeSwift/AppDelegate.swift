635f31477a8b08f5e21b76391da154bb75d744b2
Hieu Luong <dinh.hieu.luong@ing.com>
2022-03-18 08:50:46 +0100

    add more problems

--- LeetcodeSwift/AppDelegate.swift
+++ LeetcodeSwift/AppDelegate.swift
@@ -25,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func solution() {
-        Problem_2111.run()
+        Problem_1823.run()
     }
 }
 
---
+++ LeetcodeSwift/Others/Array/Problem_1823.swift
@@ -0,0 +1,32 @@
+//
+//  Problem_1823.swift
+//  LeetcodeSwift
+//
+//  Created by Hieu Luong on 18/03/2022.
+//
+
+final class Problem_1823: ProblemProtocol {
+    
+    static func run() {
+        let n = 6
+        let k = 5
+        let problem = Problem_1823()
+        let result = problem.findTheWinner(n, k)
+        print(result)
+    }
+    
+    func findTheWinner(_ n: Int, _ k: Int) -> Int {
+            
+        var arr = [Int](1...n)
+        var curIndex = 0
+        
+        while curIndex < arr.count-1 {
+            for i in 0..<k {
+                if i < k-1 { arr.append(arr[curIndex]) }
+                curIndex += 1
+            }
+        }
+        
+        return arr[curIndex]
+    }
+}
